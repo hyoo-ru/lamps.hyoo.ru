d58645c1d2952545988375a577e120f8b5bb78e3
zerkalica <zerkalica@users.noreply.github.com>
2020-03-08 07:06:16 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -4440,7 +4440,7 @@ var $;
         version() {
             return this.stat().mtime.getTime().toString(36).toUpperCase();
         }
-        exists(next) {
+        exists(next, force) {
             let exists = true;
             try {
                 this.stat();
@@ -4551,6 +4551,9 @@ var $;
     __decorate([
         $.$mol_mem
     ], $mol_file.prototype, "version", null);
+    __decorate([
+        $.$mol_mem
+    ], $mol_file.prototype, "exists", null);
     __decorate([
         $.$mol_mem
     ], $mol_file.prototype, "type", null);
