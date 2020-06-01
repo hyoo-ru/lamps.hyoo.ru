35de255a85c8fc4fa18c61ef06bad8f126ae2ac9
zerkalica <zerkalica@users.noreply.github.com>
2020-06-01 07:06:49 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -4700,6 +4700,12 @@ var $;
             version() {
                 return this.stat().mtime.getTime().toString(36).toUpperCase();
             }
+            watcher() {
+                console.warn('$mol_file_web.watcher() not implemented');
+                return {
+                    destructor() { }
+                };
+            }
             exists(next, force) {
                 let exists = true;
                 try {
@@ -4965,9 +4971,6 @@ var $;
                 if (response.native.status === 404)
                     throw new $.$mol_file_not_found(`File not found: ${this.path()}`);
                 return new Uint8Array(response.buffer());
-            }
-            watcher() {
-                throw new Error('$mol_file_web.watcher() not implemented');
             }
             stat(next, force) {
                 let stat = next;
