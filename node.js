ff8c7cffa5bdfec4332470db7affdc1c7b8e9eeb
zerkalica <zerkalica@users.noreply.github.com>
2020-10-12 07:14:25 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -4315,6 +4315,7 @@ var $;
         stat(next, force) {
             let stat = next;
             const path = this.path();
+            this.parent().watcher();
             try {
                 stat = next !== null && next !== void 0 ? next : stat_convert($node.fs.statSync(path));
             }
@@ -4324,7 +4325,6 @@ var $;
                 error.message += '\n' + path;
                 return this.$.$mol_fail_hidden(error);
             }
-            this.parent().watcher();
             return stat;
         }
         ensure(next) {
