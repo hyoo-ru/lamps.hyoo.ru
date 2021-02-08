b513a265d8232ed170c27d1cf083f713bb8d3c2d
nin-jin <nin-jin@users.noreply.github.com>
2021-02-08 07:06:58 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -6701,7 +6701,7 @@ var $;
                 flex: {
                     grow: 1000,
                     shrink: 1,
-                    basis: 'auto',
+                    basis: per(50),
                 },
             },
             Tools: {
--- web.esm.js
+++ web.esm.js
@@ -6480,7 +6480,7 @@ var $;
                 flex: {
                     grow: 1000,
                     shrink: 1,
-                    basis: 'auto',
+                    basis: per(50),
                 },
             },
             Tools: {
