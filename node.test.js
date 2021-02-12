b5b916a5a77d4f68bfc15ec9f92ea9b9b12cf8b1
nin-jin <nin-jin@users.noreply.github.com>
2021-02-12 07:07:14 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -5265,6 +5265,11 @@ var $;
         width_limit() {
             return Infinity;
         }
+        sub() {
+            return [
+                this.title()
+            ];
+        }
     }
     $.$mol_paragraph = $mol_paragraph;
 })($ || ($ = {}));
--- web.esm.js
+++ web.esm.js
@@ -5051,6 +5051,11 @@ var $;
         width_limit() {
             return Infinity;
         }
+        sub() {
+            return [
+                this.title()
+            ];
+        }
     }
     $.$mol_paragraph = $mol_paragraph;
 })($ || ($ = {}));
