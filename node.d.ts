14bfa574595b40555717bf961a892172fede8013
nin-jin <nin-jin@users.noreply.github.com>
2023-02-07 07:08:31 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1790,6 +1790,7 @@ declare namespace $ {
         line_height(): number;
         letter_width(): number;
         width_limit(): number;
+        row_width(): number;
         sub(): readonly any[];
     }
 }
